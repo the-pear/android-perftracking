7edf26301534ce5eeb2d4aa38d98b9520503e9d8
Bhat <rahul.bhat@rinmac100.intra.rakuten.co.jp>
2017-02-17 13:10:00 +0530

    Public API Integrated with Core

--- Runtime/src/main/java/jp/co/rakuten/sdtd/perf/runtime/internal/TrackingManager.java
+++ Runtime/src/main/java/jp/co/rakuten/sdtd/perf/runtime/internal/TrackingManager.java
@@ -1,32 +1,45 @@
 package jp.co.rakuten.sdtd.perf.runtime.internal;
 
+import android.content.Context;
+
+import jp.co.rakuten.sdtd.perf.core.Config;
+import jp.co.rakuten.sdtd.perf.core.Tracker;
+
 /**
  * TrackingManager
  *
  * @author RMSDK team(prj-rmsdk@mail.rakuten.com)
  */
 public class TrackingManager {
-    public static TrackingManager INSTANCE = new TrackingManager();
+    public static TrackingManager INSTANCE = null;
 
     private TrackingManager() {
+    }
+
+    public static TrackingManager getInstance(Context context, Config config) {
+        if (INSTANCE == null) {
+            Tracker.on(context, config);
+            INSTANCE = new TrackingManager();
+            return INSTANCE;
+        } else return INSTANCE;
     }
 
     /**
      * Starts a new measurement.
      *
-     * @param id Measurement identifier.
+     * @param measurementId Measurement identifier.
      */
-    public void startMeasurement(String id) {
-
+    public void startMeasurement(String measurementId) {
+        Tracker.startCustom(measurementId);
     }
 
     /**
      * Ends a measurement.
      *
-     * @param id Measurement identifier.
+     * @param trackingId Tracking ID returned from startCustom
      */
-    public void endMeasurement(String id) {
-
+    public void endMeasurement(int trackingId) {
+        Tracker.endCustom(trackingId);
     }
 
     /**
@@ -36,26 +49,24 @@ public class TrackingManager {
      * @param object Object associated with the measurement.
      */
     public void startAggregated(String id, Object object) {
-
+        Tracker.startMethod(object, id);
     }
 
     /**
      * Ends a measurement.
      *
-     * @param id     Measurement identifier.
-     * @param object Object associated with the measurement. This must be the same
-     *               object that got passed to startAggregated().
+     * @param trackingId Tracking ID returned from startMethod
      */
-    public void endAggregated(String id, Object object) {
-
+    public void endAggregated(int trackingId) {
+        Tracker.endMethod(trackingId);
     }
 
     /**
      * Starts a new metric.
      *
-     * @param id Measurement identifier.
+     * @param metricId Metric ID, for example "launch", "search", "item"
      */
-    public void startMetric(String id) {
-
+    public void startMetric(String metricId) {
+        Tracker.startMetric(metricId);
     }
 }
