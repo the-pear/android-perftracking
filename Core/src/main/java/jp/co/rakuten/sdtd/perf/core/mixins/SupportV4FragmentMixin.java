f8825d948afae5fbdf2c2c42d1985690179fa42c
Petr Luner <petr.luner@rakuten.com>
2017-03-27 11:38:37 +0900

    Fragment instrumentation

--- Core/src/main/java/jp/co/rakuten/sdtd/perf/core/mixins/SupportV4FragmentMixin.java
+++ Core/src/main/java/jp/co/rakuten/sdtd/perf/core/mixins/SupportV4FragmentMixin.java
@@ -2,9 +2,6 @@ package jp.co.rakuten.sdtd.perf.core.mixins;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import jp.co.rakuten.sdtd.perf.core.Tracker;
 import jp.co.rakuten.sdtd.perf.core.annotations.ChangeBaseTo;
 import jp.co.rakuten.sdtd.perf.core.annotations.MixSubclassOf;
