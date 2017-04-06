3e1b8d9d081402f176593fed36109766c99668c3
Gajavalli, Venkata <venkata.sg@rakuten.com>
2017-04-06 16:07:39 +0530

    [REM-20528] Corrected the missing hard coded values as per new packagename

--- Plugin/src/main/groovy/com.rakuten.tech.mobile.perf/rewriter/detours/DetourLoader.java
+++ Plugin/src/main/groovy/com.rakuten.tech.mobile.perf/rewriter/detours/DetourLoader.java
@@ -28,13 +28,13 @@ public class DetourLoader {
 			AnnotationNode a = getAnnotation(mn);
 			if (a != null) {
 				Detour detour = null;
-				if (a.desc.equals("Lcom/tech/tech/mobile/perf/core/annotations/DetourConstructorParameter;")) {
+				if (a.desc.equals("Lcom/rakuten/tech/mobile/perf/core/annotations/DetourConstructorParameter;")) {
 					detour = constructorParameterDetour(cn, mn, a);
 				}
-				else if (a.desc.equals("Lcom/tech/tech/mobile/perf/core/annotations/DetourCall;")) {
+				else if (a.desc.equals("Lcom/rakuten/tech/mobile/perf/core/annotations/DetourCall;")) {
 					detour = callDetour(cn, mn, a);
 				}
-				else if (a.desc.equals("Lcom/tech/tech/mobile/perf/core/annotations/DetourStaticCall;")) {
+				else if (a.desc.equals("Lcom/rakuten/tech/mobile/perf/core/annotations/DetourStaticCall;")) {
 					detour = staticCallDetour(cn, mn, a);
 				}
 				
