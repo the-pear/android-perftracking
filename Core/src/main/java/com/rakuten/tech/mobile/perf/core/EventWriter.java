e618b30a18766da0dfef407c61aa40149aec291d
avinash.renukaradhya@rakuten.com <avinash.renukaradhya@rakuten.com>
2017-04-11 15:44:01 +0530

    1. Escaping Double quotes. 2. replaced '.' by '#' for method separation 3. Used Variables for 'Context', 'PackageManager' and 'packageName'.
    Review Comment Fix
    
    Review Comment Fix
    
    Serialized using JsonObject calss
    
    Review comments fixed
    
    Review comments fixed
    
    Review comments fixed
    
    Review comments fixed
    
    All the changes reverted back in EventWriter and only escaping is done.

--- Core/src/main/java/com/rakuten/tech/mobile/perf/core/EventWriter.java
+++ Core/src/main/java/com/rakuten/tech/mobile/perf/core/EventWriter.java
@@ -36,24 +36,24 @@ public class EventWriter {
             _conn.connect();
 
             _writer = new BufferedWriter(new OutputStreamWriter(_conn.getOutputStream()));
-            _writer.append("{\"app\":\"").append(_config.app)
-                    .append("\",\"version\":\"").append(_config.version);
+            _writer.append("{\"app\":\"").append(escapeValue(_config.app))
+                    .append("\",\"version\":\"").append(escapeValue(_config.version));
 
             if (_envInfo.device != null) {
-                _writer.append("\",\"device\":\"").append(_envInfo.device);
+                _writer.append("\",\"device\":\"").append(escapeValue(_envInfo.device));
             }
 
             if (_envInfo.country != null) {
-                _writer.append("\",\"country\":\"").append(_envInfo.country);
+                _writer.append("\",\"country\":\"").append(escapeValue(_envInfo.country));
             }
 
             if (_envInfo.network != null) {
-                _writer.append("\",\"network\":\"").append(_envInfo.network);
+                _writer.append("\",\"network\":\"").append(escapeValue(_envInfo.network));
             }
 
             _writer.append("\",\"measurements\":[");
-
             _measurements = 0;
+
         } catch (Exception e) {
             if (_config.debug) {
                 Log.d(TAG, e.toString());
@@ -68,13 +68,11 @@ public class EventWriter {
                 if (_measurements > 0) {
                     _writer.append(',');
                 }
-
                 _writer
-                        .append("{\"metric\":\"").append(metric.id)
-                        .append("\",\"urls\":").append(Integer.toString(metric.urls))
-                        .append(",\"time\":").append(Integer.toString((int) ((metric.endTime - metric.startTime) / 1000000)))
+                        .append("{\"metric\":\"").append(escapeValue(metric.id))
+                        .append("\",\"urls\":").append(escapeValue(Integer.toString(metric.urls)))
+                        .append(",\"time\":").append(escapeValue(Integer.toString((int) ((metric.endTime - metric.startTime) / 1000000))))
                         .append('}');
-
                 _measurements++;
             } catch (Exception e) {
                 if (_config.debug) {
@@ -94,7 +92,7 @@ public class EventWriter {
 
                 switch (m.type) {
                     case Measurement.METHOD:
-                        _writer.append("{\"method\":\"").append((String) m.a).append('.').append((String) m.b).append('"');
+                        _writer.append("{\"method\":\"").append(escapeValue((String) m.a)).append('#').append(escapeValue((String) m.b)).append('"');
                         break;
 
                     case Measurement.URL:
@@ -102,25 +100,25 @@ public class EventWriter {
 
                         if (m.a instanceof URL) {
                             URL url = (URL) m.a;
-                            _writer.append(url.getProtocol()).append("://").append(url.getAuthority()).append(url.getPath());
+                            _writer.append(escapeValue(url.getProtocol())).append("://").append(escapeValue(url.getAuthority())).append(escapeValue(url.getPath()));
                         } else {
                             String url = (String) m.a;
                             int q = url.indexOf('?');
                             if (q > 0) {
                                 url = url.substring(0, q);
                             }
-                            _writer.append(url);
+                            _writer.append(escapeValue(url));
                         }
 
                         _writer.append('"');
 
                         if (m.b != null) {
-                            _writer.append(",\"verb\":\"").append((String) m.b).append('"');
+                            _writer.append(",\"verb\":\"").append(escapeValue((String) m.b)).append('"');
                         }
                         break;
 
                     case Measurement.CUSTOM:
-                        _writer.append("{\"custom\":\"").append((String) m.a).append('"');
+                        _writer.append("{\"custom\":\"").append(escapeValue((String) m.a)).append('"');
                         break;
 
                     default:
@@ -128,11 +126,9 @@ public class EventWriter {
                 }
 
                 if (metricId != null) {
-                    _writer.append(",\"metric\":\"").append(metricId).append('"');
+                    _writer.append(",\"metric\":\"").append(escapeValue(metricId)).append('"');
                 }
-
-                _writer.append(",\"time\":").append(Integer.toString((int) ((m.endTime - m.startTime) / 1000000))).append('}');
-
+                _writer.append(",\"time\":").append(escapeValue(Integer.toString((int) ((m.endTime - m.startTime) / 1000000)))).append('}');
                 _measurements++;
             } catch (Exception e) {
                 if (_config.debug) {
@@ -150,6 +146,7 @@ public class EventWriter {
                 _writer.close();
 
                 int result = _conn.getResponseCode();
+
                 if (result != 201) {
                     throw new IOException("Failed to send event with status " + result);
                 }
@@ -169,5 +166,9 @@ public class EventWriter {
         }
         _conn = null;
         _writer = null;
+    }
+
+    private String escapeValue(String s) {
+        return s.replace("\"", "\\\"");
     }
 }
