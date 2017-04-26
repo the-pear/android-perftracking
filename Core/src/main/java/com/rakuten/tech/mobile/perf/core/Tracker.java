c73cef825db6b1dacf307cedd00fa3a110c53b0e
Frey, David | Nemo <david.a.frey@rakuten.com>
2017-04-26 09:26:06 +0900

    refactor(REM-19201, Core) protect public API of `Tracker` with try-catch
    Every call to the static functions in `Tracker` is now wrapped by a
    try-catch that will handle any `Throwable` by turning off the tracking.

--- Core/src/main/java/com/rakuten/tech/mobile/perf/core/Tracker.java
+++ Core/src/main/java/com/rakuten/tech/mobile/perf/core/Tracker.java
@@ -11,6 +11,7 @@ import java.net.URL;
  * Intended to be used from instrumented code and Analytics SDK.
  * Not intended to be used directly from app.
  */
+@SuppressWarnings({"unused", "WeakerAccess"})
 public class Tracker {
 	private static TrackerImpl _tracker;
 	private static SenderThread _senderThread;
@@ -35,116 +36,160 @@ public class Tracker {
 	/**
 	 * Turns performance tracking off.
 	 */
-	public static void off()
-	{
-		_tracker = null;
-		_senderThread = null;
-	}
+    public static void off() {
+        _tracker = null;
+        _senderThread.terminate();
+        _senderThread = null;
+    }
 
-	/**
-	 * Starts metric.
-	 * @param metricId Metric ID, for example "launch", "search", "item".
-	 */
-	public static void startMetric(String metricId) {
-		TrackerImpl t = _tracker;
-		if (t != null) {
-			t.startMetric(metricId);
-		}
-	}
+    /**
+     * Starts metric.
+     * @param metricId Metric ID, for example "launch", "search", "item".
+     */
+    public static void startMetric(String metricId) {
+        try {
+            TrackerImpl t = _tracker;
+            if (t != null) {
+                t.startMetric(metricId);
+            }
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+    }
 
-	/**
-	 * Prolongs current metric.
-	 */
-	public static void prolongMetric() {
-		TrackerImpl t = _tracker;
-		if (t != null) {
-			t.prolongMetric();
-		}
-	}
+    /**
+     * Prolongs current metric.
+     */
+    public static void prolongMetric() {
+        try {
+            TrackerImpl t = _tracker;
+            if (t != null) {
+                t.prolongMetric();
+            }
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+    }
 
-	/**
-	 * Terminates current metric.
-	 */
-	public static void endMetric() {
-		TrackerImpl t = _tracker;
-		if (t != null) {
-			t.endMetric();
-		}
-	}
+    /**
+     * Terminates current metric.
+     */
+    public static void endMetric() {
+        try {
+            TrackerImpl t = _tracker;
+            if (t != null) {
+                t.endMetric();
+            }
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+    }
 
-	/**
-	 * Starts method measurement.
-	 * @param object Class instance
-	 * @param method Method name
-	 * @return Tracking ID
-	 */
-	public static int startMethod(Object object, String method) {
-		TrackerImpl t = _tracker;
-		return t != null ? t.startMethod(object, method) : 0;
-	}
+    /**
+     * Starts method measurement.
+     * @param object Class instance
+     * @param method Method name
+     * @return Tracking ID
+     */
+    public static int startMethod(Object object, String method) {
+        try {
+            TrackerImpl t = _tracker;
+            return t != null ? t.startMethod(object, method) : 0;
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+        return 0;
+    }
 
-	/**
-	 * Ends method measurement.
-	 * @param trackingId Tracking ID returned from startMethod
-	 */
-	public static void endMethod(int trackingId) {
-		TrackerImpl t = _tracker;
-		if (t != null) {
-			t.endMethod(trackingId);
-		}
-	}
+    /**
+     * Ends method measurement.
+     * @param trackingId Tracking ID returned from startMethod
+     */
+    public static void endMethod(int trackingId) {
+        try {
+            TrackerImpl t = _tracker;
+            if (t != null) {
+                t.endMethod(trackingId);
+            }
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+    }
 
-	/**
-	 * Starts URL measurement.
-	 * @param url URL
-	 * @param verb Verb, for example GET, POST, DELETE, or null
-	 * @return Tracking ID
-	 */
-	public static int startUrl(URL url, String verb) {
-		TrackerImpl t = _tracker;
-		return t != null ? t.startUrl(url, verb) : 0;
-	}
+    /**
+     * Starts URL measurement.
+     * @param url URL
+     * @param verb Verb, for example GET, POST, DELETE, or null
+     * @return Tracking ID
+     */
+    public static int startUrl(URL url, String verb) {
+        try {
+            TrackerImpl t = _tracker;
+            return t != null ? t.startUrl(url, verb) : 0;
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+        return 0;
+    }
 
-	/**
-	 * Starts URL measurement.
-	 * @param url String
-	 * @param verb Verb, for example GET, POST, DELETE, or null
-	 * @return Tracking ID
-	 */
-	public static int startUrl(String url, String verb) {
-		TrackerImpl t = _tracker;
-		return t != null ? t.startUrl(url, verb) : 0;
-	}
+    /**
+     * Starts URL measurement.
+     * @param url String
+     * @param verb Verb, for example GET, POST, DELETE, or null
+     * @return Tracking ID
+     */
+    public static int startUrl(String url, String verb) {
+        try {
+            TrackerImpl t = _tracker;
+            return t != null ? t.startUrl(url, verb) : 0;
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+        return 0;
+    }
 
-	/**
-	 * Ends URL measurement.
-	 * @param trackingId Tracking ID returned from startUrl
-	 */
-	public static void endUrl(int trackingId) {
-		TrackerImpl t = _tracker;
-		if (t != null) {
-			t.endUrl(trackingId);
-		}
-	}
+    /**
+     * Ends URL measurement.
+     * @param trackingId Tracking ID returned from startUrl
+     */
+    public static void endUrl(int trackingId) {
+        try {
+            TrackerImpl t = _tracker;
+            if (t != null) {
+                t.endUrl(trackingId);
+            }
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+    }
 
-	/**
-	 * Starts custom measurement.
-	 * @param measurementId Measurement ID
-	 * @return Tracking ID
-	 */
-	public static int startCustom(String measurementId) {
-		TrackerImpl t = _tracker;
-		return t != null ? t.startCustom(measurementId) : 0;
-	}
+    /**
+     * Starts custom measurement.
+     * @param measurementId Measurement ID
+     * @return Tracking ID
+     */
+    public static int startCustom(String measurementId) {
+        try {
+            TrackerImpl t = _tracker;
+            return t != null ? t.startCustom(measurementId) : 0;
+        } catch (Throwable t) {
+            Tracker.off();
+        }
+        return 0;
+    }
 
-	/**
-	 * Ends custom measurement.
-	 * @param trackingId Tracking ID returned from startCustom
-	 */
-	public static void endCustom(int trackingId) {
-		TrackerImpl t = _tracker;
-		if (t != null) {
-			t.endCustom(trackingId);
-		}
+    /**
+     * Ends custom measurement.
+     * @param trackingId Tracking ID returned from startCustom
+     */
+    public static void endCustom(int trackingId) {
+        try {
+            TrackerImpl t = _tracker;
+            if (t != null) {
+                t.endCustom(trackingId);
+            }
+        } catch (Throwable t) {
+            Tracker.off();
+        }
 	}
 }
