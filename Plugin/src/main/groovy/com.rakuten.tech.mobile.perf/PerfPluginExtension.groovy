59fbc2c092ea3b64b32c1fda9949a25a243c9faf
avinash.renukaradhya@rakuten.com <avinash.renukaradhya@rakuten.com>
2017-05-10 16:57:49 +0530

    [REM-20957] 1.Added a dummy rewiter which just does a copy of classes for dex conversion. 2. Checking the build type before any Task starts, Redirecting to appropriate Rewriter based on the build type.  It is a raw implementation open for ideas.

---
+++ Plugin/src/main/groovy/com.rakuten.tech.mobile.perf/PerfPluginExtension.groovy
@@ -0,0 +1,9 @@
+package com.rakuten.tech.mobile.perf;
+
+class PerfPluginExtension {
+    def String group = "jp.co.rakuten"
+    def String artifact = "library"
+    def String version = "1.0.0"
+    def String description = ""
+    def String sourceUrl = ""
+}
