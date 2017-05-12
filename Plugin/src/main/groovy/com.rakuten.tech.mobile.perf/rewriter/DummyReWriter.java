c69dc9820041b8c472d10da1f09323c4e5b9429c
Gajavalli, Venkata <venkata.sg@rakuten.com>
2017-05-12 18:30:27 +0530

    [REM-20857] Renamed classname is not updated properly, so doing it again.

--- Plugin/src/main/groovy/com.rakuten.tech.mobile.perf/rewriter/DummyReWriter.java
+++ Plugin/src/main/groovy/com.rakuten.tech.mobile.perf/rewriter/DummyReWriter.java
@@ -14,7 +14,7 @@ import java.io.File;
  * Dummy ReWriter to exclude performance tracking from debug build.
  */
 
-public class DummyRewriter implements Rewriter {
+public class DummyReWriter implements Rewriter {
 
     public String input;
     public String outputJar;
@@ -24,13 +24,13 @@ public class DummyRewriter implements Rewriter {
     public String compileSdkVersion;
     public final Logger _log;
 
-    public DummyRewriter(Logger log) {
+    public DummyReWriter(Logger log) {
         _log = log;
     }
 
     public void rewrite() {
         _log.debug(input);
-        _log.debug("DummyRewriter Populating temp JAR");
+        _log.debug("DummyReWriter Populating temp JAR");
         ClassJarMaker tempMaker = new ClassJarMaker(new File(tempJar));
         try {
             tempMaker.populate(input);
@@ -45,9 +45,9 @@ public class DummyRewriter implements Rewriter {
         try {
             ClassFilter filter = new ClassFilter();
             filter.exclude(exclude);
-            _log.info("DummyRewriter classes of : " + temp.getJarFile().getName());
+            _log.info("DummyReWriter classes of : " + temp.getJarFile().getName());
             for (String name : temp.getClasses()) {
-                _log.debug("Adding class with no DummyRewriter: " + name);
+                _log.debug("Adding class with no DummyReWriter: " + name);
                 outputMaker.add(name, temp);
             }
 
