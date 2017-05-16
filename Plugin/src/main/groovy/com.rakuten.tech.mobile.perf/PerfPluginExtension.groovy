b3407f842deab9d325c2dacd61c5cff820ba804d
avinash.renukaradhya@rakuten.com <avinash.renukaradhya@rakuten.com>
2017-05-16 11:58:53 +0530

    [REM-20957] Created extension to enable or disable diffrent build types for perf tracking

--- Plugin/src/main/groovy/com.rakuten.tech.mobile.perf/PerfPluginExtension.groovy
+++ Plugin/src/main/groovy/com.rakuten.tech.mobile.perf/PerfPluginExtension.groovy
@@ -1,9 +1,11 @@
 package com.rakuten.tech.mobile.perf;
 
 class PerfPluginExtension {
-    def String group = "jp.co.rakuten"
-    def String artifact = "library"
-    def String version = "1.0.0"
-    def String description = ""
-    def String sourceUrl = ""
+
+    String name
+    Boolean enable
+    PerfPluginExtension(final String name) {
+        this.name = name
+    }
+
 }
--- Plugin/src/main/groovy/com/rakuten/tech/mobile/perf/PerfPlugin.groovy
+++ Plugin/src/main/groovy/com/rakuten/tech/mobile/perf/PerfPlugin.groovy
@@ -1,6 +1,7 @@
 package com.rakuten.tech.mobile.perf
 
 import com.android.build.gradle.AppExtension
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -17,6 +18,15 @@ class PerfPlugin implements Plugin<Project> {
         def info = new Properties()
         info.load(PerfPlugin.class.classLoader.getResourceAsStream('info.properties'))
 
+        // Create NamedDomainObjectContainer instance for
+        // a collection of Product objects
+        NamedDomainObjectContainer<PerfPluginExtension> perfPluginExtensionContainer =
+                project.container(PerfPluginExtension)
+
+        // Add the container instance to our project
+        // with the name products.
+        project.extensions.add('performanceTracking', perfPluginExtensionContainer)
+
         def version = info.getProperty('version')
         def runtime = info.getProperty('runtime')
         def repository = info.getProperty('repository')
@@ -26,10 +36,15 @@ class PerfPlugin implements Plugin<Project> {
         def android = project.extensions.findByType(AppExtension)
         android.registerTransform(perfTrackingTransform)
 
-        // disable performance tracking for debug
+        // disable/Enabling performance tracking or build Type
+        def build = project.extensions.getByName('performanceTracking')
         project.gradle.taskGraph.beforeTask { Task task ->
             if (task.name.startsWith("transformClassesWithPerfTrackingFor")) {
-                perfTrackingTransform.setEnableReWrite(!task.name.contains("Debug"))
+                build.all{
+                    if (task.name.toLowerCase().contains(name)){
+                        mPerfTrackingTransform.setEnableReWrite(enable)
+                    }
+                }
             }
         }
 
